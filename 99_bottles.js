3578e7c57c5428d0bc15ddbf805794c69de53926
Dennis Corral <dennis.corral@outlook.com>
2022-05-24 08:01:39 -0400

    finished algo

--- 99_bottles.js
+++ 99_bottles.js
@@ -2,12 +2,26 @@ function bottleSong() {
   let originalBottleCount = 99
   let newBottleCount = 99
   let bottlesOnWall = "bottles of beer on the wall"
+  let bottleOnWall = "bottle of beer on the wall"
+  let takeAndPass = "Take one down and pass it around"
 
   while(newBottleCount > 1){
-    console.log(`${newBottleCount} ${bottlesOnWall},${newBottleCount} bottles of beer.\n Take one down and pass it around, ${newBottleCount-1} ${bottlesOnWall}.`)
-    newBottleCount--
+      if(newBottleCount >2){
+        console.log(`${newBottleCount} ${bottlesOnWall}, ${newBottleCount} bottles of beer.\n ${takeAndPass}, ${newBottleCount-1} ${bottlesOnWall}.`)
+      newBottleCount--
+      }else{
+      console.log(`${newBottleCount} ${bottlesOnWall}, ${newBottleCount} bottles of beer.\n ${takeAndPass}, ${newBottleCount-1} ${bottleOnWall}.`)
+      newBottleCount--}
   }
+  
+  console.log(`${newBottleCount} ${bottleOnWall}, ${newBottleCount} bottle of beer.\n ${takeAndPass}, no more ${bottlesOnWall}.`)
+  console.log(`No more ${bottlesOnWall}, no more bottles of beer.`)
+  console.log(`Go to the store and buy some more, 99 ${bottlesOnWall}.`)
+      
+    
+  
 
 };
+
 
 bottleSong()
